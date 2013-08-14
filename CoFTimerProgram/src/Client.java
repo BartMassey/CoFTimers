ff72a2d1806e56e3ddf4a6ce022db60345ec381e
Vikram Ramesh <vikram132@gmail.com>
2013-08-14 00:36:34 -0700

    added functionality for rest of timers

--- CoFTimerProgram/src/Client.java
+++ CoFTimerProgram/src/Client.java
@@ -32,6 +32,8 @@ import java.awt.event.ActionEvent;
 import java.awt.*;
 import javax.swing.*;
 import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.*;
 
 //import com.google.gson.JsonArray;
@@ -45,24 +47,21 @@ public class Client extends JFrame implements ActionListener {
 
 	final String baseURL = "https://api.guildwars2.com";
 	//create the various GUI elements
-	JLabel d1Label = new JLabel("Ascalon Catacombs");
-	JLabel d2Label = new JLabel("Caudecus's Manor");
-	JLabel d3Label = new JLabel("Twilight Arbor");
-	JLabel d4Label = new JLabel("Sorrow's Embrace");
-	JLabel d5Label = new JLabel("Citadel of Flame");
-	JLabel d6Label = new JLabel("Honor of the Waves");
-	JLabel d7Label = new JLabel("Crucible of Eternity");
-	JLabel d8Label = new JLabel("The Ruined City of Arah");
+	JLabel d1Label = new JLabel("Temple of Balthazar");
+	JLabel d2Label = new JLabel("Temple of Dwayna");
+	JLabel d3Label = new JLabel("Temple of Grenth");
+	JLabel d4Label = new JLabel("Temple of Lyssa");
+	JLabel d5Label = new JLabel("Temple of Melandru");
+	JLabel d6Label = new JLabel("Citadel of Flame");
+
 	
-	JLabel d1StatusLabel = new JLabel("YES");
-	JLabel d2StatusLabel = new JLabel("YES");
-	JLabel d3StatusLabel = new JLabel("YES");
-	JLabel d4StatusLabel = new JLabel("YES");
-	JLabel d5StatusLabel = new JLabel("YES");
-	JLabel d6StatusLabel = new JLabel("YES");
-	JLabel d7StatusLabel = new JLabel("YES");
-	JLabel d8StatusLabel = new JLabel("YES");
-	
+	JLabel d1StatusLabel = new JLabel("---");
+	JLabel d2StatusLabel = new JLabel("---");
+	JLabel d3StatusLabel = new JLabel("---");
+	JLabel d4StatusLabel = new JLabel("---");
+	JLabel d5StatusLabel = new JLabel("---");
+	JLabel d6StatusLabel = new JLabel("---");
+
 	JButton refreshButton = new JButton("Refresh");
 	JLabel selectServerLabel = new JLabel("Select Server");
 	JButton launchOverlayButton = new JButton("Launch Overlay");
@@ -70,10 +69,12 @@ public class Client extends JFrame implements ActionListener {
 				 "Devona's Rest", "Dragonbrand", "Ehmry Bay", "Eredon Terrace",
 				 "Ferguson's Crossing", "Fort Aspenwood", "Gate of Madness", 
 				 "Henge of Denravi", "Isle of Janthir", "Jade Quarry", "Kaineng", "Maguuma",
-				 "Northern Shiverpeaks", "Sanctum of Rail", "Sea of Sorrows", 
+				 "Northern Shiverpeaks", "Sanctum of Rall", "Sea of Sorrows", 
 				 "Sorrow's Furnace", "Stormbluff Isle", "Tarnished Coast", "Yak's Bend"};
 
 	JComboBox serverList = new JComboBox(serverNames);
+	
+	Map<String, Integer> serverCodes = new HashMap<String,Integer>();
 
 
 	public static void main(String args[]){
@@ -86,12 +87,45 @@ public class Client extends JFrame implements ActionListener {
 		
 		initializeBasicUI();
 		sendHTTPRequest();
-		refresh(1001, 22, "A1182080-2599-4ACC-918E-A3275610602B");
+		refreshAll(1001);
+	
 		//more todo client code here
 	}
 
 	//use this method to initially load the world/server/event ids TODO
 	public void sendHTTPRequest(){
+		
+		//---------------------------------FOR DEMO----------------------
+		//Manually map the world codes. Make it automated later once json parsing is properly integrated
+		serverCodes.put("Anvil Rock", 1001);
+		serverCodes.put("Blackgate", 1019);
+		serverCodes.put("Borlis Pass", 1002);
+		serverCodes.put("Crystal Desert", 1014);
+		serverCodes.put("Darkhaven", 1012);
+		serverCodes.put("Devona's Rest", 1023);
+		serverCodes.put("Dragonbrand", 1021);
+		serverCodes.put("Ehmry Bay", 1010);
+		serverCodes.put("Eredon Terrace", 1024);
+		serverCodes.put("Ferguson's Crossing", 1020);
+		serverCodes.put("Fort Aspenwood", 1009);
+		serverCodes.put("Gate of Madness", 1007);
+		serverCodes.put("Henge of Denravi", 1004);
+		serverCodes.put("Isle of Janthir", 1015);
+		serverCodes.put("Jade Quarry", 1008);
+		serverCodes.put("Kaineng", 1022);
+		serverCodes.put("Maguuma", 1005);
+		serverCodes.put("Northern Shiverpeaks", 1018);
+		serverCodes.put("Sanctum of Rall", 1013);
+		serverCodes.put("Sea of Sorrows", 1016);
+		serverCodes.put("Sorrow's Furnace", 1006);
+		serverCodes.put("Stormbluff Isle", 1011);
+		serverCodes.put("Tarnished Coast", 1017);
+		serverCodes.put("Yak's Bend", 1003);
+		
+		
+		//---------------------------------------------------------------
+		
+		
 	
 	try{
 		
@@ -118,7 +152,7 @@ public class Client extends JFrame implements ActionListener {
 	}
 
 	//every time program refreshes to repull data, use this
-	public void refresh(int worldID, int mapID, String eventID){
+	public void refresh(int worldID, int mapID, String eventID, JLabel statusLabel){
 
 		//anvil rock id: 1001
 		//fireheart rise id: 22
@@ -142,6 +176,8 @@ public class Client extends JFrame implements ActionListener {
 			System.exit(0);
 		}
 		
+
+		
 		//System.out.println(eventString);
 		//System.out.println(eventString.charAt(eventString.indexOf("A1182080-2599-4ACC-918E-A3275610602B") + 47));
 
@@ -151,14 +187,14 @@ public class Client extends JFrame implements ActionListener {
 		}else{
 
 			char status = eventString.charAt(eventString.indexOf(eventID) + 47);
-			System.out.println(status);
+			//System.out.println(status);
 
 			if (status == 'S'){
-				d5StatusLabel.setText("Open");
+				statusLabel.setText("Open");
 			}else if(status == 'A'){
-				d5StatusLabel.setText("Event Up");
+				statusLabel.setText("Event Up");
 			}else{
-				d5StatusLabel.setText("Closed");
+				statusLabel.setText("Closed");
 			}
 		}
 	}
@@ -175,27 +211,25 @@ public class Client extends JFrame implements ActionListener {
 
 		//label containers
 		Container leftContainer = new Container();
-		leftContainer.setLayout(new GridLayout(8,1));
+		leftContainer.setLayout(new GridLayout(6,1));
 		leftContainer.add(d1Label);
 		leftContainer.add(d2Label);
 		leftContainer.add(d3Label);
 		leftContainer.add(d4Label);
 		leftContainer.add(d5Label);
 		leftContainer.add(d6Label);
-		leftContainer.add(d7Label);
-		leftContainer.add(d8Label);
+
 		
 		//status containers
 		Container rightContainer = new Container();
-		rightContainer.setLayout(new GridLayout(8,1));
+		rightContainer.setLayout(new GridLayout(6,1));
 		rightContainer.add(d1StatusLabel);
 		rightContainer.add(d2StatusLabel);
 		rightContainer.add(d3StatusLabel);
 		rightContainer.add(d4StatusLabel);
 		rightContainer.add(d5StatusLabel);
 		rightContainer.add(d6StatusLabel);
-		rightContainer.add(d7StatusLabel);
-		rightContainer.add(d8StatusLabel);
+
 
 		Container bottomContainer = new Container();
 		bottomContainer.setLayout(new GridLayout(1,4));
@@ -230,15 +264,38 @@ public class Client extends JFrame implements ActionListener {
 
 		refreshButton.addActionListener((ActionListener) this);
 		launchOverlayButton.addActionListener((ActionListener) this);
+		serverList.addActionListener((ActionListener) this);
 	}
 
 	public void actionPerformed(ActionEvent e){
 		
+		if (e.getSource() == serverList){
+			//System.out.println(serverList.getSelectedItem());
+			//if (serverList.getSelectedItem().equals("Anvil Rock"));
+			//System.out.println(mapCodes.get(serverList.getSelectedItem()));
+			int serverID = serverCodes.get(serverList.getSelectedItem());
+			refreshAll(serverID);
+	
+			
+		}
+		
 		if (e.getSource() == refreshButton){
 			d1StatusLabel.setText("Refresh works");
+			int serverID = serverCodes.get(serverList.getSelectedItem());
+			refreshAll(serverID);
 		}
 
 	}//end actionListeners
 
+	//refresh all of the temple timers
+	public void refreshAll(int serverID){
+	
+		refresh(serverID, 51, "2555EFCB-2927-4589-AB61-1957D9CC70C8", d1StatusLabel);//balthazar
+		refresh(serverID, 65, "6A6FD312-E75C-4ABF-8EA1-7AE31E469ABA", d2StatusLabel);//dwayna
+		refresh(serverID, 62, "99254BA6-F5AE-4B07-91F1-61A9E7C51A51", d3StatusLabel);//grenth
+		refresh(serverID, 65, "0372874E-59B7-4A8F-B535-2CF57B8E67E4", d4StatusLabel);//lyssa
+		refresh(serverID, 62, "A5B5C2AF-22B1-4619-884D-F231A0EE0877", d5StatusLabel);//melandru
+		refresh(serverID, 22, "A1182080-2599-4ACC-918E-A3275610602B", d6StatusLabel);//cof
+	}
 	
 }
